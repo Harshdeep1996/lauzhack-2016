8d1cd8578c7898609b2ccbe0eb0159b032412af9
Harshdeep1996 <harsh.deep@student.manchester.ac.uk>
2016-11-20 02:48:19 +0100

    Getting json output

--- Barcode_Reader/app/src/main/java/com/example/harshdeepharshdeep/barcode_reader/MainActivity.java
+++ Barcode_Reader/app/src/main/java/com/example/harshdeepharshdeep/barcode_reader/MainActivity.java
@@ -2,9 +2,17 @@ package com.example.harshdeepharshdeep.barcode_reader;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.zxing.integration.android.IntentIntegrator;
 import com.google.zxing.integration.android.IntentResult;
 import android.content.Intent;
+import android.util.Log;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -14,43 +22,68 @@ import android.widget.Toast;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.logging.Logger;
+
 
 public class MainActivity extends AppCompatActivity implements OnClickListener {
     private Button scanBtn;
     private TextView formatTxt, contentTxt;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        NukeSSLCerts.nuke();
         setContentView(R.layout.activity_main);
-        scanBtn = (Button)findViewById(R.id.scan_button);
-        formatTxt = (TextView)findViewById(R.id.scan_format);
-        contentTxt = (TextView)findViewById(R.id.scan_content);
+        scanBtn = (Button) findViewById(R.id.scan_button);
+        formatTxt = (TextView) findViewById(R.id.scan_format);
+        contentTxt = (TextView) findViewById(R.id.scan_content);
         scanBtn.setOnClickListener(this);
     }
-    public void onClick(View v)
-    {
-        if(v.getId() == R.id.scan_button)
-        {
+
+    public void onClick(View v) {
+        if (v.getId() == R.id.scan_button) {
             IntentIntegrator scanIntegrator = new IntentIntegrator(this);
             scanIntegrator.initiateScan();
         }
     }
-    public void onActivityResult(int requestCode, int resultCode, Intent intent)
-    {
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent) {
+        String scanContent = "", scanFormat = "";
         IntentResult scanningResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, intent);
         if (scanningResult != null)
         {
-            String scanContent = scanningResult.getContents();
-            String scanFormat = scanningResult.getFormatName();
+            scanContent = scanningResult.getContents();
+            scanFormat = scanningResult.getFormatName();
             formatTxt.setText("FORMAT: " + scanFormat);
             contentTxt.setText("CONTENT: " + scanContent);
-        }
-        else
-        {
+        } else {
             Toast toast = Toast.makeText(getApplicationContext(),
                     "No scan data received!", Toast.LENGTH_SHORT);
             toast.show();
-            }
         }
+
+
+        final TextView mTextView = (TextView) findViewById(R.id.scan_format);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "https://www.openfood.ch/api/v1/products?barcodes[]=3046920022651";
+        Log.v("main", url);
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        Log.v("main", "RESPONSE RECEIVED");
+                        // Display the first 500 characters of the response string.
+                        mTextView.setText("Response is: " + response.substring(0, 500));
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.v("main", "RESPONSE NOT WORK " + error.getMessage());
+                mTextView.setText("That didn't work!");
+            }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+    }
 }
